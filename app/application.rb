6b1db4c41495f918efba859adf8cb4423e073e6a
Adam Duff <j.adam.duff@gmail.com>
2018-08-31 21:00:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      if @@items == []
+      if @@cart == []
         resp.write "Your cart is empty"
       else
         @@cart.each do |x|
